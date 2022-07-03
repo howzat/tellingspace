052ef75d69cabf9b57226f92fbaf8d1e4e089975
Ben Letton <benletton@gmail.com>
2022-07-03 22:43:18 +0100

    Rename some things to make the world less confusing (fix)

--- cicd/bin/bootstrap.ts
+++ cicd/bin/bootstrap.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {BuildConfig, GithubSourceConfig, PipelineConfig, WebsitePipeline} from "../lib/pipeline";
+import {GithubSourceConfig, PipelineConfig, WebsitePipeline} from "../lib/pipeline";
 
 const app = new cdk.App();
 const appName = "ToldSpaces"
 const sourceOwner = app.node.tryGetContext('sourceOwner');
 const sourceRepositoryName = app.node.tryGetContext('sourceRepository');
 const sourceConfig = new GithubSourceConfig("main", sourceOwner, sourceRepositoryName);
-const pipelineConfig = new PipelineConfig(appName, sourceConfig, new BuildConfig());
+const pipelineConfig = new PipelineConfig(appName, sourceConfig);
 
 let env = {
     account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
