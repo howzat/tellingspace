e0f84c4296dff23d53a454d82fd4635b8aa0acab
Ben Letton <benletton@gmail.com>
2022-07-08 11:03:18 +0100

    Passing Unit Test for Navigation

--- webapp/toldspaces/src/__tests__/SiteNavigation.test.tsx
+++ webapp/toldspaces/src/__tests__/SiteNavigation.test.tsx
@@ -3,14 +3,22 @@
  */
 import React from "react";
 import {Navigation} from "../components/Navigation";
-import {render, screen} from "@testing-library/react";
+import TestRenderer from 'react-test-renderer';
+import {Link} from "gatsby"; // ES6
 
 describe("<Navigation />", () => {
     it("should display a list of links with the right 'to' fields", async () => {
+        let testRenderer = TestRenderer.create(<Navigation
+          links={[{to: "toValue1", text: "textValue1"}, {to: "toValue2", text: "textValue2"}]}/>);
+        let root = testRenderer.root;
 
-        let nav = render(<Navigation links={[]}/>);
-        const links = screen.getByTestId("links");
-        console.log(nav)
-        // ???
+        let findAllByType = root.findAllByType(Link);
+        expect(findAllByType[0].props.to).toBe("toValue1")
+        expect(findAllByType[0].props.children).toHaveLength(2)
+        expect(findAllByType[0].props.children).toContain("textValue1")
+
+        expect(findAllByType[1].props.to).toBe("toValue2")
+        expect(findAllByType[1].props.children).toHaveLength(2)
+        expect(findAllByType[1].props.children).toContain("textValue2")
     });
 });
