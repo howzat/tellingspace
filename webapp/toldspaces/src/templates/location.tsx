1ba2530cc133a03c24e6ab85966b42262c83b1bd
Ben Letton <benletton@gmail.com>
2022-07-09 13:05:08 +0100

    Nodejs 14 again, darn AWS

--- webapp/toldspaces/src/templates/location.tsx
+++ webapp/toldspaces/src/templates/location.tsx
@@ -1,40 +1,17 @@
 import React from "react"
 import {MainLayout} from "../layouts/MainLayout";
-import {NavigationProps} from "../components/Navigation";
-import {graphql} from "gatsby";
+import {graphql, PageProps} from "gatsby";
 import flatprint from "../flatprint";
 
-export type LocationTemplateProps = {
-  data: LocationProps
-  pageContext?: any
-}
-
-export type LocationProps = {
-  sids: Array<string>
-  sid: string
-}
-
-const LocationTemplate = ({data, pageContext}: LocationTemplateProps) => {
+const LocationTemplate = ({data, pageContext}: PageProps<Queries.LocationsJson>) => {
 
     flatprint("data", data)
     flatprint("pageContext", pageContext)
-
-    let items: NavigationProps = {
-        links: [{
-            text: "element 1",
-            to: "string 1",
-        }, {
-            text: "element 2",
-            to: "string 2",
-        }]
-    }
-
-    console.log("LocationTemplate items", items)
     return (
       <>
-          <h1>So this is christmas</h1>
-          <p>And what have we done</p>
-          <MainLayout><h1>Location Page </h1></MainLayout>
+          <MainLayout>
+              <h1>Location Page </h1>
+          </MainLayout>
       </>
     )
 }
@@ -42,11 +19,19 @@ const LocationTemplate = ({data, pageContext}: LocationTemplateProps) => {
 export default LocationTemplate
 
 export const query = graphql`
-  query queryBySid($sid:String) {
-      allLocationsJson(filter: {sid: {eq: $sid}}) {
-          nodes {
-              sid
-          }
-      }
-  }
+    query queryBySid($sid:String) {
+        allLocationsJson(filter: {sid: {eq: $sid}}) {
+            nodes {
+                sid
+                name
+                text
+                image
+                description
+                coords {
+                    lat
+                    lng
+                }
+            }
+        }
+    }
 `
